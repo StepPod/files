81543996b0cd6a664150b59c65a9315d093213a8
dokebiCEO <dokebivc@gmail.com>
2024-11-28 17:38:25 +0900

    dcgm exporter의 values.yaml 추가 및 release=prometheus 라벨 추가

--- dcgm-exporter/apply.sh
+++ dcgm-exporter/apply.sh
@@ -1,3 +1,4 @@
 helm repo add gpu-helm-charts https://nvidia.github.io/dcgm-exporter/helm-charts
 helm repo update
-helm install --generate-name gpu-helm-charts/dcgm-exporter
+
+helm install --generate-name gpu-helm-charts/dcgm-exporter -f ./files/dcgm-exporter/values.yaml --namespace=metrics-monitoring
